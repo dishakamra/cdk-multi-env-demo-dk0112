dced6be3f4f7ff011f7698b1e48bb214c8c03a2d
Disha Kamra <dishakamra@gmail.com>
2025-12-01 04:37:31 -0500

    Initial commit

---
+++ test/cdk-multi-env-demo-dk0112.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkMultiEnvDemoDk0112 from '../lib/cdk-multi-env-demo-dk0112-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-multi-env-demo-dk0112-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkMultiEnvDemoDk0112.CdkMultiEnvDemoDk0112Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
